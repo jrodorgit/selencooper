a64f34857e637f6518a84adcb3bf60cc69024891
52535465S <52535465S@DES-62396.trabajo.dom>
2019-06-26 13:33:54 +0200

    mas refact

---
+++ TestFuncCooperativas/src/test/java/net/rodor/testfuncooper/soldenom/SuiteTestRegresionSolDenom.java
@@ -0,0 +1,19 @@
+package net.rodor.testfuncooper.soldenom;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	TestRegresionSolDenomOnline.class,
+	TestRegresionSolProrrogaDenomOnline.class,
+	TestRegresionSolDenomManual.class,
+	TestRegresionGestionCertDenom.class
+	
+})
+
+public class SuiteTestRegresionSolDenom {
+	
+	
+
+}
