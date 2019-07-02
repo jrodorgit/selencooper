33432c4b7a2650fa11274fd744ff63e9eb7513bd
52535465S <52535465S@DES-62396.trabajo.dom>
2019-07-02 09:06:26 +0200

    nuevos tests

---
+++ TestFuncCooperativas/src/test/java/net/rodor/testfuncooper/test/regresion/legajos/SuiteTestRegresionLegajos.java
@@ -0,0 +1,15 @@
+package net.rodor.testfuncooper.test.regresion.legajos;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	
+	TestRegresionGrabarLegajo.class,
+	TestRegresionEliminarLegajo.class
+	
+})
+public class SuiteTestRegresionLegajos {
+
+}
--- TestFuncCooperativas/src/test/java/net/rodor/testfuncooper/test/regresion/legajos/TestRegresionEliminarLegajo.java
+++ TestFuncCooperativas/src/test/java/net/rodor/testfuncooper/test/regresion/legajos/TestRegresionEliminarLegajo.java
@@ -11,32 +11,29 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import net.rodor.testfuncooper.OPAccesoChrome;
 import net.rodor.testfuncooper.asientos.VOAsiento;
 import net.rodor.testfuncooper.legajos.OPAltaLegajo;
-import net.rodor.testfuncooper.legajos.OPAsociacionLegajoAsiento;
 import net.rodor.testfuncooper.legajos.OPDetalleLegajo;
-import net.rodor.testfuncooper.legajos.VODocumento;
+import net.rodor.testfuncooper.legajos.OPListadoLegajo;
 import net.rodor.testfuncooper.legajos.VOLegajo;
 import net.rodor.testfuncooper.test.regresion.TestRegresionBase;
 
-public class TestRegresionGrabarLegajo extends TestRegresionBase {
+public class TestRegresionEliminarLegajo extends TestRegresionBase {
 
 	
 	VOLegajo legajo = null;
-	VODocumento doc = null;
 	VOAsiento asiento = null;
 	
 	
 	@Before
 	public void inicializaTest() throws InterruptedException, AWTException{
 
-		System.out.println("Inicializando TestRegresionGrabarLegajo....\n");
+		System.out.println("Inicializando TestRegresionEliminarLegajo....\n");
 		
 		inicializaEntorno();
 		
 		ApplicationContext context = new ClassPathXmlApplicationContext(
 				"net/rodor/testfuncooper/test/regresion/legajos/data_set_legajos_sp_config.xml");
 		
-		legajo = (VOLegajo) context.getBean("legajo");
-		doc = (VODocumento) context.getBean("documento");
+		legajo = (VOLegajo) context.getBean("legajoEliminar");
 		asiento = (VOAsiento) context.getBean("asiento");
 		
 		driver = OPAccesoChrome.autenticacion(env.getProps().get("URL_PRIV"));
@@ -46,26 +43,17 @@ public class TestRegresionGrabarLegajo extends TestRegresionBase {
 	@Test
 	public void testRegresion() throws InterruptedException{
 		
-		System.out.println("Lanzando TestRegresionGrabarLegajo....\n");
+		System.out.println("Lanzando TestRegresionEliminarLegajo....\n");
 		
 		OPAltaLegajo.alta(driver,legajo);
 		
-		OPDetalleLegajo.modificarLegajo(driver,legajo,asiento);
+		OPListadoLegajo.consultarLegajo(driver, legajo, asiento);
 		
-		OPDetalleLegajo.addDocALegajo(driver,legajo, doc);
-		
-		OPDetalleLegajo.visualizarDocLegajo(driver,legajo, doc);
-		
-		OPDetalleLegajo.modificaDescripcionDocLegajo(driver,legajo, doc);
-		
-		OPAsociacionLegajoAsiento.addAsientoALegajo(driver, legajo, asiento);
-		
+		OPDetalleLegajo.deleteLegajo(driver, legajo);
 	}
 	
 	@After
 	public void finaliza(){
-		finalizaEntorno();
+		//finalizaEntorno();
 	}
-	
-	
 }
